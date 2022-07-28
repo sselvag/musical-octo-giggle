2e0849acda93148a916a65c5847be483bf200ee8
sselvag <swaathy.selva@gmail.com>
2022-07-28 13:46:38 -0400

    added header and homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import TopBar from "./topbar/topbar";
+import Home from "./pages/home/Home";
+import TopBar from "./topbar/TopBar";
 
 
 function App() {
   return (
     <>
-      <TopBar />
+      <TopBar/>
+      <Home/>
     </>
   );
 }
