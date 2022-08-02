d2a6c7ccc6ed2c7b2d0e82af70633e9a6bddd4ea
Eric Mok <ericmok@Erics-MacBook-Air.local>
2022-08-02 12:39:31 -0400

    update

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
 
 const postSchema = new Schema({
     postText: {
@@ -17,7 +16,6 @@ const postSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (timestamp) => dateFormat(timestamp),
     },
     comments: [
         {
@@ -30,7 +28,6 @@ const postSchema = new Schema({
             createdAt: {
                 type: Date,
                 default: Date.now,
-                get: (timestamp) => dateFormat(timestamp),
             },
         },
     ],
