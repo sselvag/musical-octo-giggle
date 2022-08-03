832b3bbb2664c6df3fcef10ef51a2e3d740f1c4e
Swaathy <swaathy.selva@gmail.com>
2022-08-02 23:31:17 -0400

    router works

--- myserver/cclient/src/App.js
+++ myserver/cclient/src/App.js
@@ -6,22 +6,59 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Authors from "./components/Authors";
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        authors: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        posts: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
 
 const client = new ApolloClient({
   uri:'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 
 function App() {
   return (
-    <>
       <ApolloProvider client={client}>
-        <NavBar/>
-        <Authors/>
+      <Router>
+        <>
+          <NavBar/>
+          <Routes>
+
+            <Route path='/' element={<Home/>} />
+
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+
+            <Route path='/authors' element={<Authors/>} />
+
+            <Route path='/write' element={<Write/>} />
+
+            <Route path='/post/:postId' element={<Single/>} />
+
+          </Routes>
+        </>
+      </Router>
       </ApolloProvider>
-    </>
+
   );
 }
 
--- myserver/cclient/src/components/Header.js
+++ myserver/cclient/src/components/Header.js
@@ -2,12 +2,14 @@ import "../styles/header.css"
 
 export default function Header(){
     return (
-        <div class="header">
-            <div class="headerTitles">
-                <span class="headerTitleSm">Journals of the</span>
-                <span class="headerTitleLg">Wanderlust</span>
+        <>
+            <div class="header">
+                <div class="headerTitles">
+                    <span class="headerTitleSm">Journals of the</span>
+                    <span class="headerTitleLg">Wanderlust</span>
+                </div>
+                <img class="headerImage" src="https://wallpaperaccess.com/full/2577607.jpg" alt="Hero Image"/>
             </div>
-            <img class="headerImage" src="https://wallpaperaccess.com/full/2577607.jpg" alt="Hero Image"/>
-        </div>
+        </>
     )
 }
