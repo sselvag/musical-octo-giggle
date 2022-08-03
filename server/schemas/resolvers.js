7eff050d04eacc2780798fa40f27f47c79081f95
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-08-03 00:15:45 -0400

    post added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -33,5 +33,65 @@ const resolvers = {
 
 			return usersPosts;
 		},
+    },
+
+    Mutation: {
+        // Creates a New User
+        createUser: async (parent, {username, first_name, last_name, email, password}) =>
+        {
+            const newUser = new User ({
+                username, 
+                first_name,
+                last_name,
+                email,
+                password,
+            });
+
+            const user = await newUser.save();
+        },
+
+        // Logs in to the User Account 
+        login: async (parent, { username, password}) => {
+
+            // Incorrect Email or Password
+            const errorMessage = 'Incorrect Email or Password !';
+
+            // Finds a Userin the Database
+            const user = await User.findOne({username});
+
+            // No User Found
+            if (!user) {
+                throw new AuthenticationError(errorMessage)
+            }
+
+            return {user};
+        },
+
+        //add a new Post to our database
+		addPost: async (parent, { body, user_id }) => {
+			//create new model for our db
+			const newPost = new Post({ body, user_id });
+
+			//save model to database
+			const Post = await newPost.save();
+
+			//return Post
+			return Post;
+		},
+
+		//update Post
+		updatePost: async (parent, { id, body }) => {
+			const updatedPost = await Post.findByIdAndUpdate(id, { body });
+
+			return updatedPost;
+		},
+
+		//delete Post
+		deletePost: async (parent, { id }) => {
+			//get Post by id and delete
+			const deletePost = await Post.findByIdAndDelete(id);
+
+			return true;
+		},
     }
 }
